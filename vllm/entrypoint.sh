316c5f5b9abc8984c87ed30562d0bd487a54eda3
timmson <timmson666@mail.ru>
2026-04-15 01:29:10 +0300

    Add to qwen3.5:9b

--- vllm/entrypoint.sh
+++ vllm/entrypoint.sh
@@ -14,23 +14,28 @@ if [ -z "$TP_SIZE" ]; then
 fi
 
 # 2. Настройка параметров по умолчанию
-GPU_UTIL=${GPU_UTIL:-0.95}
-MAX_MODEL_LEN=${MAX_MODEL_LEN:-16384}
+MODEL="qwen3.5:2b"
+PORT=11434
+GPU_UTIL=${GPU_UTIL:-0.8}
+MAX_MODEL_LEN=${MAX_MODEL_LEN:-65536}
 DTYPE=${DTYPE:-"auto"}
 TOOLS=${TOOLS:-"--enable-auto-tool-choice --tool-call-parser hermes"}
 EXTRA_ARGS=${EXTRA_ARGS:-""}
 
 # 3. Вывод итоговой конфигурации для логов
 echo "Starting vLLM with:"
+echo " - Model: $MODEL"
+echo " - Port: $PORT"
 echo " - Tensor Parallel Size: $TP_SIZE"
 echo " - GPU Utilization: $GPU_UTIL"
 echo " - Max Context Length: $MAX_MODEL_LEN"
+echo " - Tools: $TOOLS"
 echo " - Extra arguments: $EXTRA_ARGS"
 
 # 4. Запуск vLLM
 exec vllm serve "/app/model" \
-    --port 11434 \
-    --served-model-name "qwen3.5:4b" \
+    --port $PORT \
+    --served-model-name "$MODEL" \
     --tensor-parallel-size "$TP_SIZE" \
     --gpu-memory-utilization "$GPU_UTIL" \
     --max-model-len "$MAX_MODEL_LEN" \
